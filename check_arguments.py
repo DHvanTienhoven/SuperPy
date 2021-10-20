da5da4f710bd06570af4f52d1a6fbb44fe04e3d9
Darya van Tienhoven <daryaheleen@gmail.com>
2021-10-20 17:49:10 +0200

    description added check_arguments.py

--- check_arguments.py
+++ check_arguments.py
@@ -1,4 +1,7 @@
 def check_arguments(product_name, price):
+    '''
+    Description: this function is called on when a product or a sale is made and will warn the user if one or more of the required arguments (product name and price) is missing
+    '''
     if (product_name == None and price==None):
         raise AttributeError("please provide the product name with the --name or -n flag and the price with the --price or -p flag")
     if product_name == None:
